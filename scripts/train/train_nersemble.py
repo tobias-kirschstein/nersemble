7a78f0c02c7a2768242e3fd4e95ffb4bd89792dc
Tobias Kirschstein <tobias.kirschstein@gmail.com>
2024-04-15 23:28:23 +0200

    Support resuming checkpoints

--- scripts/train/train_nersemble.py
+++ scripts/train/train_nersemble.py
@@ -22,6 +22,8 @@ from nersemble.nerfstudio.models.nersemble_instant_ngp import NeRSembleNGPModelC
 
 from torchinfo import summary
 
+from nersemble.util.setup import try_load_config
+
 # [left, forward, down], [right, backward, up]
 # [[-2.5, -2, -2.5], [2.5, 3, 2]]]
 # SCENE_BOXES = {
@@ -103,7 +105,10 @@ def main(
 
         # View Frustum Culling
         use_view_frustum_culling: bool = True,
-        view_frustum_culling: int = 2
+        view_frustum_culling: int = 2,
+
+        resume_run: Optional[str] = None,
+        resume_checkpoint: Optional[int] = None,
 
 ):
     os.environ['WANDB_RUN_GROUP'] = "nersemble"
@@ -128,128 +133,139 @@ def main(
         # [[left, head front, down], [right, head back, up]]
         scene_box = torch.tensor([[-2.5, -2, -2.5], [2.5, 3, 2]]) * scale_factor / 9
 
-    config = NeRSembleTrainerConfig(
-        project_name="nersemble",
-        experiment_name=run_name,
-        method_name="nersemble",
+    if resume_run:
+        existing_model_manager = NeRSembleModelFolder().open_run(resume_run)
+        config = try_load_config(existing_model_manager.get_config_path())
+        config.experiment_name = run_name
+        config.run_name = run_name
+        config.output_dir = Path(model_folder.get_location())
 
-        output_dir=Path(model_folder.get_location()),
-        run_name=run_name,
+        config.load_dir = Path(existing_model_manager.get_checkpoint_folder())
+        config.load_step = resume_checkpoint
+    else:
+        config = NeRSembleTrainerConfig(
+            project_name="nersemble",
+            experiment_name=run_name,
+            method_name="nersemble",
 
-        steps_per_eval_batch=500,
-        steps_per_eval_image=steps_per_eval_image,
-        steps_per_eval_all_images=steps_per_eval_all_images,
-        steps_per_save=50000,
-        max_num_iterations=300001,
-        save_only_latest_checkpoint=True,
-        mixed_precision=True,
-        log_gradients=False,
+            output_dir=Path(model_folder.get_location()),
+            run_name=run_name,
 
-        pipeline=VanillaPipelineConfig(
-            datamanager=NeRSembleVanillaDataManagerConfig(
-                dataparser=NeRSembleDataParserConfig(
-                    participant_id=participant_id,
-                    sequence_name=sequence_name,
-                    start_timestep=start_timestep,
-                    n_timesteps=n_timesteps,
-                    skip_timesteps=skip_timesteps,
-                    scale_factor=scale_factor,
-                    scene_box=scene_box,
+            steps_per_eval_batch=500,
+            steps_per_eval_image=steps_per_eval_image,
+            steps_per_eval_all_images=steps_per_eval_all_images,
+            steps_per_save=50000,
+            max_num_iterations=300001,
+            save_only_latest_checkpoint=True,
+            mixed_precision=True,
+            log_gradients=False,
 
+            pipeline=VanillaPipelineConfig(
+                datamanager=NeRSembleVanillaDataManagerConfig(
+                    dataparser=NeRSembleDataParserConfig(
+                        participant_id=participant_id,
+                        sequence_name=sequence_name,
+                        start_timestep=start_timestep,
+                        n_timesteps=n_timesteps,
+                        skip_timesteps=skip_timesteps,
+                        scale_factor=scale_factor,
+                        scene_box=scene_box,
+
+                    ),
+                    train_num_rays_per_batch=n_train_rays,
+                    eval_num_rays_per_batch=1024,
+                    train_num_images_to_sample_from=24,
+                    train_num_times_to_repeat_images=20,
+                    eval_num_images_to_sample_from=36,
+                    use_cache_compression=False,
+                    max_cached_items=max_cached_images,  # 10k should be roughly 200GB
                 ),
-                train_num_rays_per_batch=n_train_rays,
-                eval_num_rays_per_batch=1024,
-                train_num_images_to_sample_from=24,
-                train_num_times_to_repeat_images=20,
-                eval_num_images_to_sample_from=36,
-                use_cache_compression=False,
-                max_cached_items=max_cached_images,  # 10k should be roughly 200GB
+
+                model=NeRSembleNGPModelConfig(
+                    # Ray Marching
+                    render_step_size=0.011 * scale_factor / 9.,  # TODO: Is render_step_size correct?
+                    near_plane=0.2 * scale_factor / 9.,
+                    far_plane=1e3 * scale_factor / 9.,
+                    cone_angle=cone_angle,
+                    alpha_thre=alpha_thre,  # TODO: Do lower values help? It seems like the occupancy grid is too aggressive
+                    occ_thre=occ_thre,
+                    early_stop_eps=0,  # Important, otherwise scene may start exploding
+                    background_color="white",
+                    grid_levels=grid_levels,
+                    # Originally, NeRSemble was trained with a single grid, but larger values also work
+                    disable_scene_contraction=True,  # To ensure scene only exists inside scene box
+                    max_n_samples_per_batch=-1 if max_n_samples_per_batch == -1 else 2 ** max_n_samples_per_batch,
+
+                    # Sequence
+                    n_timesteps=n_timesteps,
+                    latent_dim_time=latent_dim_time,  # If hash ensemble is used, this must be n_hash_encodings!
+
+                    # Losses
+                    use_masked_rgb_loss=True,
+                    alpha_mask_threshold=0,
+                    lambda_alpha_loss=lambda_alpha_loss,
+                    lambda_near_loss=lambda_near_loss,
+                    lambda_empty_loss=lambda_empty_loss,
+                    lambda_depth_loss=lambda_depth_loss,
+                    lambda_dist_loss=lambda_dist_loss,
+
+                    # Hash Ensemble
+                    use_hash_ensemble=use_hash_ensemble,
+                    hash_ensemble_config=HashEnsembleConfig(
+                        n_hash_encodings=n_hash_encodings,
+                        hash_encoding_config=TCNNHashEncodingConfig(),
+                        disable_initial_hash_ensemble=True,
+                        use_soft_transition=True
+                    ),
+
+                    # Deformation Field
+                    use_deformation_field=use_deformation_field,
+                    use_separate_deformation_time_embedding=True,
+                    deformation_field_config=SE3DeformationFieldConfig(
+                        warp_code_dim=latent_dim_time_deform,
+                        mlp_num_layers=mlp_num_layers,
+                        mlp_layer_width=mlp_layer_width,
+                    ),
+                    disable_occupancy_grid=disable_occupancy_grid,
+
+                    # Scheduler
+                    window_hash_encodings_begin=window_hash_encodings_begin,
+                    window_hash_encodings_end=window_hash_encodings_end,
+                    window_deform_begin=window_deform_begin,
+                    window_deform_end=window_deform_end,
+
+                    # View Frustum Culling
+                    use_view_frustum_culling=use_view_frustum_culling,
+                    view_frustum_culling=view_frustum_culling,
+                )
             ),
 
-            model=NeRSembleNGPModelConfig(
-                # Ray Marching
-                render_step_size=0.011 * scale_factor / 9.,  # TODO: Is render_step_size correct?
-                near_plane=0.2 * scale_factor / 9.,
-                far_plane=1e3 * scale_factor / 9.,
-                cone_angle=cone_angle,
-                alpha_thre=alpha_thre,  # TODO: Do lower values help? It seems like the occupancy grid is too aggressive
-                occ_thre=occ_thre,
-                early_stop_eps=0,  # Important, otherwise scene may start exploding
-                background_color="white",
-                grid_levels=grid_levels,
-                # Originally, NeRSemble was trained with a single grid, but larger values also work
-                disable_scene_contraction=True,  # To ensure scene only exists inside scene box
-                max_n_samples_per_batch=-1 if max_n_samples_per_batch == -1 else 2 ** max_n_samples_per_batch,
-
-                # Sequence
-                n_timesteps=n_timesteps,
-                latent_dim_time=latent_dim_time,  # If hash ensemble is used, this must be n_hash_encodings!
-
-                # Losses
-                use_masked_rgb_loss=True,
-                alpha_mask_threshold=0,
-                lambda_alpha_loss=lambda_alpha_loss,
-                lambda_near_loss=lambda_near_loss,
-                lambda_empty_loss=lambda_empty_loss,
-                lambda_depth_loss=lambda_depth_loss,
-                lambda_dist_loss=lambda_dist_loss,
-
-                # Hash Ensemble
-                use_hash_ensemble=use_hash_ensemble,
-                hash_ensemble_config=HashEnsembleConfig(
-                    n_hash_encodings=n_hash_encodings,
-                    hash_encoding_config=TCNNHashEncodingConfig(),
-                    disable_initial_hash_ensemble=True,
-                    use_soft_transition=True
-                ),
-
-                # Deformation Field
-                use_deformation_field=use_deformation_field,
-                use_separate_deformation_time_embedding=True,
-                deformation_field_config=SE3DeformationFieldConfig(
-                    warp_code_dim=latent_dim_time_deform,
-                    mlp_num_layers=mlp_num_layers,
-                    mlp_layer_width=mlp_layer_width,
-                ),
-                disable_occupancy_grid=disable_occupancy_grid,
-
-                # Scheduler
-                window_hash_encodings_begin=window_hash_encodings_begin,
-                window_hash_encodings_end=window_hash_encodings_end,
-                window_deform_begin=window_deform_begin,
-                window_deform_end=window_deform_end,
-
-                # View Frustum Culling
-                use_view_frustum_culling=use_view_frustum_culling,
-                view_frustum_culling=view_frustum_culling,
-            )
-        ),
-
-        optimizers={
-            "fields": {
-                "optimizer": AdamOptimizerConfig(lr=lr_main, eps=1e-15, weight_decay=0),
-                "scheduler": StepLRSchedulerConfig(step_size=20000, gamma=8e-1)
+            optimizers={
+                "fields": {
+                    "optimizer": AdamOptimizerConfig(lr=lr_main, eps=1e-15, weight_decay=0),
+                    "scheduler": StepLRSchedulerConfig(step_size=20000, gamma=8e-1)
+                },
+                "deformation_field": {
+                    "optimizer": AdamOptimizerConfig(lr=lr_deformation_field, eps=1e-15, weight_decay=0),
+                    "scheduler": StepLRSchedulerConfig(step_size=20000, gamma=5e-1),
+                },
+                "embeddings": {
+                    "optimizer": AdamOptimizerConfig(lr=lr_embeddings, eps=1e-15, weight_decay=0),
+                    "scheduler": StepLRSchedulerConfig(step_size=20000, gamma=8e-1),
+                },
             },
-            "deformation_field": {
-                "optimizer": AdamOptimizerConfig(lr=lr_deformation_field, eps=1e-15, weight_decay=0),
-                "scheduler": StepLRSchedulerConfig(step_size=20000, gamma=5e-1),
-            },
-            "embeddings": {
-                "optimizer": AdamOptimizerConfig(lr=lr_embeddings, eps=1e-15, weight_decay=0),
-                "scheduler": StepLRSchedulerConfig(step_size=20000, gamma=8e-1),
-            },
-        },
 
-        viewer=ViewerConfig(num_rays_per_chunk=1 << 15),
-        vis=vis,
-    )
+            viewer=ViewerConfig(num_rays_per_chunk=1 << 15),
+            vis=vis,
+        )
 
-    dataparser_config: NeRSembleDataParserConfig = config.pipeline.datamanager.dataparser
-    dataparser_config.use_alpha_maps = lambda_alpha_loss > 0
-    dataparser_config.use_depth_maps = lambda_empty_loss > 0 or lambda_near_loss > 0 or lambda_depth_loss > 0
-    dataparser_config.use_view_frustum_culling = config.pipeline.model.use_view_frustum_culling
+        dataparser_config: NeRSembleDataParserConfig = config.pipeline.datamanager.dataparser
+        dataparser_config.use_alpha_maps = lambda_alpha_loss > 0
+        dataparser_config.use_depth_maps = lambda_empty_loss > 0 or lambda_near_loss > 0 or lambda_depth_loss > 0
+        dataparser_config.use_view_frustum_culling = config.pipeline.model.use_view_frustum_culling
 
-    config.set_timestamp()
+        config.set_timestamp()
+
     # print config
     config.print_to_terminal()
 
